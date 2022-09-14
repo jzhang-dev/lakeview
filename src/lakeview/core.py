16d8bc96b17cc9e28c6b56e7f17c241353212b23
Jia-Yuan Zhang <sloppybaron@gmail.com>
2022-09-14 16:49:54 +0000

    Add DotPlot

--- src/lakeview/core.py
+++ src/lakeview/core.py
@@ -6,4 +6,5 @@ __email__ = "jzhang@well.ox.ac.uk"
 
 from . import util
 from .alignment import AlignedSegment, SequenceAlignment, GeneAnnotation
+from .sequences import DotPlot
 from .widget import GenomeViewer
--- src/lakeview/util.py
+++ src/lakeview/util.py
@@ -13,16 +13,83 @@ def base_formatter(unit="mb", fmt="{:.2f}"):
     return unit_formatter
 
 
-def complement_base(base):
-    if base == "A":
-        return "T"
-    if base == "T":
-        return "A"
-    if base == "G":
-        return "C"
-    if base == "C":
-        return "G"
-    if base == "N":
-        return "N"
-    else:
+# def complement_base(base):
+#     if base == "A":
+#         return "T"
+#     if base == "T":
+#         return "A"
+#     if base == "G":
+#         return "C"
+#     if base == "C":
+#         return "G"
+#     if base == "N":
+#         return "N"
+#     else:
+#         raise ValueError
+
+
+def init_reverse_complement():
+    TRANSLATION_TABLE = str.maketrans("ACTGactg", "TGACtgac")
+
+    def reverse_complement(sequence):
+        """
+        >>> reverse_complement("AATC")
+        'GATT'
+        >>> reverse_complement("CCANT")
+        'ANTGG'
+        """
+        sequence = str(sequence)
+        return sequence.translate(TRANSLATION_TABLE)[::-1]
+
+    return reverse_complement
+
+
+reverse_complement = init_reverse_complement()
+
+
+def is_canonical(kmer):
+    """
+    >>> is_canonical("ATC")
+    True
+    >>> is_canonical("GGC")
+    False
+    """
+    return kmer <= reverse_complement(kmer)
+
+
+def canonicalize(kmer):
+    canonical_kmer = min(kmer, reverse_complement(kmer))
+    return canonical_kmer
+
+
+def iterate_kmers(sequence, k, strand="single", drop_N=True):
+    """
+    >>> list(iterate_kmers("AATGANGGG", 3, 'canonical'))
+    ['AAT', 'ATG', 'TCA', 'CCC']
+    """
+    for __, kmer in enumerate_kmers(sequence, k, strand, drop_N):
+        yield kmer
+
+
+def enumerate_kmers(sequence, k, strand="single", drop_N=True):
+    """
+    >>> list(enumerate_kmers("AATGANGGG", 3, 'canonical'))
+    [(0, 'AAT'), (1, 'ATG'), (2, 'TCA'), (6, 'CCC')]
+    """
+    if len(sequence) < k:
         raise ValueError
+    if strand not in ("single", "canonical"):
+        raise ValueError
+
+    sequence = str(sequence).upper()
+    l = len(sequence)
+    for i in range(l):
+        if i + k > l:
+            return
+        kmer = sequence[i : i + k]
+        if drop_N and "N" in kmer:
+            continue
+        if strand == "single":
+            yield i, kmer
+        elif strand == "canonical":
+            yield i, canonicalize(kmer)
